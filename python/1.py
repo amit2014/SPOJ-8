2337f6f741e6d473ea6cc68b6fdca0282ab81a67
Heitor Silva <heitor-silva@hotmail.com>
2012-05-08 19:39:37 -0300

    Adding solutions for problem 1 using Python and Ruby

---
+++ python/1.py
@@ -0,0 +1,7 @@
+# Life, the Universe, and Everything
+
+num = int(raw_input(''))
+
+while(num!=42):
+    print num
+    num = int(raw_input(''))
---
+++ ruby/1.rb
@@ -0,0 +1,8 @@
+# Life, the Universe, and Everything
+
+num = gets.to_i
+
+while num != 42
+  puts num
+  num = gets.to_i
+end
